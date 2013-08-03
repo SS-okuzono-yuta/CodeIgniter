9ae3549c8916d0fd8ada57c02bac918f8b24ee07
Shintaro Kaneko <kaneshin0120@gmail.com>
2013-08-03 18:50:29 +0900

    Add my core classes for controller and model

---
+++ application/core/MY_Controller.php
@@ -0,0 +1,20 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Extended Controller Core Class
+ */
+class MY_Controller extends CI_Controller {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+// END Extended Controller Core Class
+
+/* End of file MY_Controller.php */
+/* Location: ./application/core/MY_Controller.php */
---
+++ application/core/MY_Model.php
@@ -0,0 +1,20 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Extended Model Core Class
+ */
+class MY_Model extends CI_Model {
+
+	/**
+	 * Constructor
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
+
+}
+// END Extended Model Core Class
+
+/* End of file MY_Model.php */
+/* Location: ./application/core/MY_Model.php */
