fb8aa4a9c9f22e076d25fdea193008dc71de11b1
Shintaro Kaneko <kaneshin0120@gmail.com>
2013-12-21 13:17:02 +0000

    Update Basic_auth Class

---
+++ application/config/admin.php
@@ -0,0 +1,13 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Admin
+|--------------------------------------------------------------------------
+|
+*/
+$config['admin_user'] = '';
+$config['admin_password'] = '';
+
+/* End of file admin.php */
+/* Location: ./application/config/admin.php */
--- application/libraries/Basic_auth.php
+++ application/libraries/Basic_auth.php
@@ -2,14 +2,13 @@
 
 /**
  * File:        Basic_auth
- * Version:     0.2
+ * Version:     0.3
  * Maintainer:  Shintaro Kaneko <kaneshin0120@gmail.com>
- * Last Change: 15-Sep-2013.
+ * Last Change: 21-Dec-2013.
  */
 class Basic_auth {
 
-	private $_default_user = 'admin';
-	private $_default_password = '';
+	private $CI;
 
 	private $_admin_user;
 	private $_admin_password;
@@ -18,20 +17,26 @@ class Basic_auth {
 
 	public function __construct()
 	{
-		$config =& get_config();
-		$this->_admin_user = (isset($config['admin_user']))? $config['admin_user'] : $this->_default_user;
-		$this->_admin_password = (isset($config['admin_password'])) ? $config['admin_password'] : $this->_default_password;
+		$this->CI =& get_instance();
+		$this->CI->config->load('admin');
+		$this->_admin_user = $this->CI->config->item('admin_user');
+		$this->_admin_password = $this->CI->config->item('admin_password');
+
+		if ($this->_admin_user == '')
+		{
+			show_error('In order to use the Basic_auth class you are required to set an admin user in your config file.');
+		}
 	}
 
 	public function login()
 	{
-		$CI =& get_instance();
-		$logged_in = $CI->session->userdata($this->_logged_in_key);
+		$this->CI->load->library('session');
+		$logged_in = $this->CI->session->userdata($this->_logged_in_key);
 		if ($logged_in !== TRUE)
 		{
 			if ($this->headers_401())
 			{
-				$CI->session->set_userdata(array(
+				$this->CI->session->set_userdata(array(
 					$this->_logged_in_key => TRUE
 				));
 			}
@@ -44,20 +49,16 @@ class Basic_auth {
 
 	private function headers_401()
 	{
-		$CI =& get_instance();
 		$user = $this->_admin_user;
 		$password = $this->_admin_password;
-		if (!$CI->input->server('PHP_AUTH_USER'))
+
+		if (($this->CI->input->server('PHP_AUTH_USER') === $user)
+			&& ($this->CI->input->server('PHP_AUTH_PW') === $password))
 		{
+			return TRUE;
+		} else {
 			header('WWW-Authenticate: Basic realm="SMF Studios"');
 			header('HTTP/1.0 401 Unauthorized');
-		}
-		else
-		{
-			if (($CI->input->server('PHP_AUTH_USER') === $user) && ($CI->input->server('PHP_AUTH_PW') === $password))
-			{
-				return TRUE;
-			}
 		}
 		return FALSE;
 	}
